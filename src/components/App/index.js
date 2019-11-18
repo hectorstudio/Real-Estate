06c6710f4e8c8f662d9050a82b1cb4b7a3032afd
hectorstudio <david.liu987@hotmail.com>
2019-11-18 12:48:07 +0000

    Add SASS style support

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import s from './index.module.scss';
+
+function App() {
+  return (
+    <div className={s.root}>
+      App
+    </div>
+  );
+}
+
+export default App;
