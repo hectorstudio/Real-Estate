b271bbd64874497e6e2d79d28e18148fb0e882c5
hectorstudio <david.liu987@hotmail.com>
2020-03-14 09:44:06 +0800

    Initialization Project

--- src/constants/validation/helpers.js
+++ src/constants/validation/helpers.js
@@ -1,5 +1,6 @@
 import * as yup from 'yup';
 
+
 // Phone number
 const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 export const phoneNumber = yup.string().matches(phoneRegExp, 'Phone number is not valid');
